c21c7637b5e273c9639029783b5ef63d3de9e755
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2012-12-27 16:26:28 +0100

    Removed protected workspace

--- workspace/HelloColibri/cooja_infomem.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void infomem_read(void *buffer, unsigned int offset, unsigned char size) {
-	printf("infomem_read o:%d l:%d\n",offset,size);
-}
-void infomem_write(void *buffer, unsigned int offset, unsigned char size) {
-	printf("infomem_read o:%d l:%d\n",offset,size);
-}
