07e241c826087e91c0ca22c02690b04ef0c4097c
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2013-08-06 12:22:35 +0200

    Rimosso debug dentro nullrdc Utilizzo put char da USB con buffer

--- platform/colibri/usb0-putchar.c
+++ platform/colibri/usb0-putchar.c
@@ -4,19 +4,30 @@
 #include "USB/usbConstructs.h"
 #include <descriptors.h>
 
+#ifdef SERIAL_LINE_CONF_BUFSIZE
+#define BUFSIZE SERIAL_LINE_CONF_BUFSIZE
+#else /* SERIAL_LINE_CONF_BUFSIZE */
+#define BUFSIZE 128
+#endif /* SERIAL_LINE_CONF_BUFSIZE */
+
+#if (BUFSIZE & (BUFSIZE - 1)) != 0
+#error SERIAL_LINE_CONF_BUFSIZE must be a power of two (i.e., 1, 2, 4, 8, 16, 32, 64, ...).
+#error Change SERIAL_LINE_CONF_BUFSIZE in contiki-conf.h.
+#endif
+
 //extern uint8_t usb_printf_state;
-uint8_t buffer[64];
-uint8_t bufPos=0;
+uint8_t outBuff[BUFSIZE];
+uint8_t outBuffPos=0;
 
 int putchar(int c) {
-	uint8_t ch = (uint8_t)c;
+	//uint8_t ch = (uint8_t)c;
     //if(usb_printf_state == USB_DISABLED) return -1;
     //if(usb_printf_state == USB_LOCKED) return -1;
     //usb_printf_state = USB_LOCKED;
-	buffer[bufPos++]=c;
-	if(c=='\n' || bufPos == 64){
-		cdcSendDataWaitTilDone(buffer,bufPos, CDC0_INTFNUM, 1);
-		bufPos=0;
+	outBuff[outBuffPos++] = c;
+	if(c=='\n'||outBuffPos==64) {
+		cdcSendDataWaitTilDone(outBuff,outBuffPos, CDC0_INTFNUM, 1);
+		outBuffPos=0;
 	}
 	//usb_printf_state = USB_ENABLED;
 	return c;
