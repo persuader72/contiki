9a51ece315050ec31b5c2c7641dc9ee818fef0fe
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2013-09-10 10:13:25 +0200

    Riaggiuntoi modulo per la gestione del pwm

---
+++ platform/colibri/dev/pwm.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012, Andrea Cannavicci + Stefano Pagnottelli
+ * CBL Electronics srl + Siralab srl
+ * */
+#include "contiki.h"
+#include "pwm.h"
+
+
+void pwm_init(void){
+
+	//moved to ioInit
+	  //P1SEL |= BIT1 | BIT2 | BIT3;  // P1.2 TA0.1
+	  //P1DIR |= BIT1 | BIT2 | BIT3;  // P1.2 TA0.1
+
+	  TA0CCR0 = 128;                            // PWM Period/2
+
+	  TA0CCTL1 = OUTMOD_7;                      // CCR1 toggle/set
+	  TA0CCR1 = 32;                             // CCR1 PWM duty cycle
+
+	  TA0CCTL2 = OUTMOD_7;                      // CCR2 toggle/set
+	  TA0CCR2 = 96;                             // CCR2 PWM duty cycle
+	  TA0CTL = TASSEL_2 | MC_1 | TACLR;         // SMCLK, up-down mode, clear TAR
+
+}
+
+void pwm_set(uint16_t pwmPeriod, uint16_t pwmDuty[]){
+	  TA0CCR0 = pwmPeriod;                       // PWM Period
+	  TA0CCR1 = pwmDuty[0];                      // CCR1 PWM duty cycle
+	  TA0CCR2 = pwmDuty[1];                      // CCR2 PWM duty cycle
+
+}
